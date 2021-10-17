d31e6a4edf285460a2705a525e876818a8a7f9ac
Bill Lytton <wwlytton@yahoo.com>
2021-10-17 13:43:24 -0400

    will need to redo the arguments

--- sobol.py
+++ sobol.py
@@ -8,7 +8,7 @@ from itertools import product
 def parseBatchParams (b):
     ''' read a batch.py file for NetPyNE param search; returning list of (name, valueList, [indexed]) where optional indexed means to use all the values '''
     with open(b,'r') as fb: lines = fb.readlines()
-    p = re.compile('''\s +params[^a-z]+([^]']+)'\]\s *=\s *(\[[^]]+\])\s *#*\s *(indexed)*''') # indexed is the keyword to NOT sobolize eg amp or cellnum
+    p = re.compile('''\s +params[^a-z]+([^]']+)'\]\s *=\s *(\[[^]]+\])\s *#*\s *(indexed)*''') # 'indexed' is the keyword to NOT sobolize eg amp or cellnum
     bl, pl = [(i, p.match(l)) for i,l in enumerate(lines)], [] # bl: lines that match regexp
     for i,m in bl:
         if m:
@@ -20,16 +20,24 @@ def parseBatchParams (b):
         
 def sobcall (pl, num, seed=1234):
     ''' determine the min, max of sobolized params and do the combos with indexed params '''
-    mins, maxs = [(min(eval(x[1])), max(eval(x[1]))) for x in pl if not x[2]]
-    svals = qmc.scale(sob(len(mins), num, seed=seed), mins, maxs)
+    labels, mins, maxs = [], [], []
+    for x in pl:
+        if not x[2]:
+            labels.append(x[0]); mins.append(min(x[1])); maxs.append(max(x[1]))
+    print('mins/maxs for %s: '%(labels), mins, maxs)
+    vals = sob(len(mins), num, seed=seed)
+    svals = qmc.scale(vals, mins, maxs) # only for those that are not 'indexed'
+    
     return svals
     
 def sob (dim=4, num=4096, f=None, seed=1234):
     sm = sbl(d=dim, scramble=True, seed=seed)
     m = math.floor(math.log(num)/math.log(2) + 0.99) # round up to nearest power of 2
-    if 2**m != num: print('Saving %d samples (2^%d ; %d requested)'%(2**m, m, num))
+    if 2**m != num: print('%d samples (2^%d ; %d requested)'%(2**m, m, num))
     vals = sm.random_base2(m=m) # 2^m points
-    if f: np.savetxt(f, vals, delimiter=',', fmt='%.10f')
+    if f: 
+        print('saving raw sobol values to %s'%(f))
+        np.savetxt(f, vals, delimiter=',', fmt='%.10f')
     return vals
 
 def getArgs ():
@@ -37,15 +45,14 @@ def getArgs ():
     msg = "Generate at least cnt lines of sobol samples of size dim and place in file sobol.out"
     parser = argparse.ArgumentParser(description=msg, )    
     parser.add_argument('dim', nargs='?', type=int, default=4, help='dim of space to be sampled')
-    parser.add_argument('cnt', nargs='?', type=int, default=16, help='num of samples: will be rounded up to a power of 2')
-    parser.add_argument("-o", default='sobol.csv', help='output filename (default ./sobol.csv)')
+    parser.add_argument('cnt', nargs='?', type=int, default=10, help='num of samples: will be rounded up to a power of 2')
+    parser.add_argument("-r", default='sobol.csv', help='raw output from sobol call (default ./sobol.csv)')
+    parser.add_argument("-f", default='params.csv', help='file for saving param lists (default ./params.csv)')
     parser.add_argument("-s", default=1234, type=int, help='seed')
     parser.add_argument("-v", action='store_true', default=False, help='output to terminal')
-    parser.add_argument("-b", default='batch.py', help='name of batchfile (default ./batch.py')
+    parser.add_argument("-b", default='batch.py', help='name of batchfile with "params" ranges (default ./batch.py)')
     return parser.parse_args()
 
 if __name__ == '__main__':
     ag = getArgs()
-    sobcall(parseBatchParams(ag.b))
-    # vals = sob(dim=ag.dim, num=ag.cnt, seed=ag.s, f=ag.o if ag.o else None)
-
+    svals = sobcall(parseBatchParams(ag.b), ag.cnt)
